b6d471422f09b4354ee458db5b6fef6af06c6619
p1ju5 <pijusmisiunas17@gmail.com>
2022-10-20 13:10:51 +0300

    test

--- Saitynai/Data/DemoRestContext.cs
+++ Saitynai/Data/DemoRestContext.cs
@@ -12,7 +12,7 @@ namespace Saitynai.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // !!! DON'T STORE THE REAL CONNECTION STRING THE IN PUBLIC REPO !!!
-            // Use secret managers provided by your chosen cloud provider
+            // Use secret managers provided by your chosen cloud provide
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=RestDemo");
         }
     }
